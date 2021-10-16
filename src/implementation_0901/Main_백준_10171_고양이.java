057b69a730e916c88d13f5b1c4f38585851fd15e
이채하 <chaeha93@gmail.com>
2021-10-16 23:52:06 +0900

    Just playing

---
+++ src/implementation_0901/Main_백준_10171_고양이.java
@@ -0,0 +1,10 @@
+package implementation_0901;
+
+public class Main_백준_10171_고양이 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
---
+++ src/implementation_0901/Main_백준_10718_Welovekriii.java
@@ -0,0 +1,8 @@
+package implementation_0901;
+
+public class Main_백준_10718_Welovekriii {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
