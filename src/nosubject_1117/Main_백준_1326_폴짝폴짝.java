6ef4cb61ba316a6d9230124c9b5516139cdbe2b7
이채하 <chaeha93@gmail.com>
2021-11-23 16:58:42 +0900

    Solve BOJ 1326 폴짝폴짝

--- src/nosubject_1117/Main_백준_1326_폴짝폴짝.java
+++ src/nosubject_1117/Main_백준_1326_폴짝폴짝.java
@@ -1,3 +1,4 @@
+package nosubject_1117;
 import java.io.*;
 import java.util.*;
 
@@ -24,24 +25,28 @@ public class Main_백준_1326_폴짝폴짝 {
 
 	private static int bfs() {
 		Queue<int[] > queue = new LinkedList<int[] >();
+		boolean[] visited = new boolean[N];
 		queue.offer(new int[] {start, 0});
+		visited[start] = true;
 		
 		while(!queue.isEmpty()) {
 			int[] p = queue.poll();
-			if(p[0] == end) {
-				return p[1];
-			}
+			
 			int idx = 1;
-			for(int i=arr[p[0]]; i<N; i=arr[p[0]]*idx) {
-				idx++;
-				queue.offer(new int[] {i, p[1]+1});
-				if(p[0]-i >= 0) {
-					queue.offer(new int[] {p[0]-i, p[1]+1});
+			for(int i=arr[p[0]]; i<N; i=idx*arr[p[0]]) {
+				if(i+p[0] == end || p[0]-i == end) return p[1]+1;
+				if(i+p[0] <N && !visited[i + p[0]]) { // 오른쪽 이동
+					queue.offer(new int[] {i+p[0], p[1]+1});
+					visited[i+p[0]] = true;					
 				}
+				if(p[0]-i >= 0 && !visited[p[0]-i]) { // 왼쪽 이동
+					queue.offer(new int[] {p[0]-i, p[1]+1});
+					visited[p[0]-i] = true;
+				}
+				idx++;
 			}
 		}
 		return -1;
-		
 	}
 
 }
