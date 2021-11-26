74b041fbe8a0ed5e05e5a85d6ab0ee39da781df6
이채하 <chaeha93@gmail.com>
2021-11-26 13:14:59 +0900

    Solve Math problems

---
+++ src/implementation_0901/Main_백준_3003_킹퀸룩비숍나이트폰.java
@@ -0,0 +1,18 @@
+package implementation_0901;
+import java.io.*;
+import java.util.*;
+
+public class Main_백준_3003_킹퀸룩비숍나이트폰 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] ans = {1, 1, 2, 2, 2, 8};
+		for(int i=0; i<6; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			sb.append(ans[i]-num).append(" ");
+		}
+		System.out.println(sb);
+	}
+
+}
