f79da5fa8b35fdc61dcc21bb50c5ee4cec0d0af3
이채하 <chaeha93@gmail.com>
2022-12-21 22:46:38 +0900

    Solve BOJ 10926 ??!

---
+++ src/implementation_221221/Main_백준_10926_.java
@@ -0,0 +1,11 @@
+package implementation_221221;
+
+import java.io.*;
+
+public class Main_백준_10926_ {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s + "??!");
+	}
+}
