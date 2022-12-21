532f47f82ed547cc7112e5972420192555199776
이채하 <chaeha93@gmail.com>
2022-12-21 22:53:12 +0900

    Solve BOJ 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/implementation_221221/Main_백준_18108.java
@@ -0,0 +1,12 @@
+package implementation_221221;
+
+import java.io.*;
+
+public class Main_백준_18108 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int num = 2541 - 1998;
+		System.out.println(a - num);
+	}
+}
